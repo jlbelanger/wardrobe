dccd63c6b2254057de8c163916f33fdd45f1a889
Jenny Belanger <jbelangr@gmail.com>
2022-03-25 18:41:40 -0400

    Lint

--- config/cache.php
+++ config/cache.php
@@ -61,9 +61,6 @@ return [
 				env('MEMCACHED_USERNAME'),
 				env('MEMCACHED_PASSWORD'),
 			],
-			'options' => [
-				// Memcached::OPT_CONNECT_TIMEOUT => 2000,
-			],
 			'servers' => [
 				[
 					'host' => env('MEMCACHED_HOST', '127.0.0.1'),
@@ -105,6 +102,6 @@ return [
 	|
 	*/
 
-	'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
+	'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_cache'),
 
 ];
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,8 @@ class UserFactory extends Factory
 			'name' => $this->faker->name(),
 			'email' => $this->faker->unique()->safeEmail(),
 			'email_verified_at' => now(),
-			'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+			// Password: 'password'.
+			'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
 			'remember_token' => Str::random(10),
 		];
 	}
@@ -30,7 +31,7 @@ class UserFactory extends Factory
 	 */
 	public function unverified()
 	{
-		return $this->state(function (array $attributes) {
+		return $this->state(function () {
 			return [
 				'email_verified_at' => null,
 			];
