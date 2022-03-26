a4f60c3712d3d1de0019a5a8a214bc912ab65be5
Jenny Belanger <jbelangr@gmail.com>
2022-03-26 17:21:45 -0400

    Add error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<title>404 Not Found | Jenny's Wardrobe</title>
+		<link rel="icon" href="/favicon.svg">
+		<link rel="stylesheet" href="/assets/css/style.min.css?20220325">
+	</head>
+	<body>
+		<div id="hangers">
+			<div class="hanger"></div><div class="hanger"></div><div class="hanger"></div><div class="hanger"></div>
+			<div class="hanger"></div><div class="hanger"></div><div class="hanger"></div><div class="hanger"></div>
+			<div class="hanger"></div><div class="hanger"></div><div class="hanger"></div><div class="hanger"></div>
+			<div class="hanger"></div><div class="hanger"></div><div class="hanger"></div><div class="hanger"></div>
+			<div class="hanger"></div><div class="hanger"></div><div class="hanger"></div><div class="hanger"></div>
+		</div>
+		<main id="container">
+			<h1>Not Found</h1>
+			<p>The requested URL was not found on this server.</p>
+		</main>
+	</body>
+</html>
