b281109d519dbf0a8949db04627f9b3c16e7ebb8
Jenny Belanger <jbelangr@gmail.com>
2022-03-26 11:39:55 -0400

    Set tapioca password reset URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Http\Kernel;
 use App\Models\Clothes;
 use App\Observers\ClothesObserver;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,6 +30,11 @@ class AppServiceProvider extends ServiceProvider
 		if ($this->app->environment() !== 'local') {
 			$kernel->appendMiddlewareToGroup('api', \Illuminate\Routing\Middleware\ThrottleRequests::class);
 		}
+
+		// phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.FoundInExtendedClassBeforeLastUsed
+		ResetPassword::createUrlUsing(function ($notifiable, string $token) {
+			return env('ADMIN_URL') . '/reset-password/' . $token;
+		});
 
 		Clothes::observe(ClothesObserver::class);
 	}
