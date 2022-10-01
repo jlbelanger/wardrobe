8a143509976ea6dab1f6bd643184f1fd01263274
Jenny Belanger <jbelangr@gmail.com>
2022-09-30 21:43:08 -0400

    Rename uploaded files to match new name

--- app/Observers/ClothesObserver.php
+++ app/Observers/ClothesObserver.php
@@ -10,6 +10,33 @@ class ClothesObserver
 	 * @param  Clothes $clothes
 	 * @return void
 	 */
+	public function updating(Clothes $clothes)
+	{
+		if (!$clothes->isDirty('name')) {
+			return;
+		}
+
+		// Rename files to match new name.
+		if ($clothes->filename) {
+			$oldFilename = $clothes->filename;
+			$newFilename = $clothes->uploadedFilename('filename', $oldFilename);
+			$oldPath = public_path($oldFilename);
+			$newPath = public_path($newFilename);
+			if (file_exists($oldPath) && !file_exists($newPath)) {
+				$folder = preg_replace('/\/[^\/]+$/', '', $newPath);
+				if (!is_dir($folder)) {
+					mkdir($folder);
+				}
+				rename($oldPath, $newPath);
+				$clothes->filename = $newFilename;
+			}
+		}
+	}
+
+	/**
+	 * @param  Clothes $clothes
+	 * @return void
+	 */
 	public function updated(Clothes $clothes)
 	{
 		// When uploading or removing file, delete the old file.
